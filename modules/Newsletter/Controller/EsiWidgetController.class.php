0c656232596857d59636717752d0fcb5dcbeea0e
Mirjam Doyon <mirjam.doyon@comvation.com>
2019-09-20 08:48:06 +0200

    feat(newsletter): initialize widget newsletter-Form (CLX-2827)

---
+++ modules/Newsletter/Controller/EsiWidgetController.class.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Cloudrexx
+ *
+ * @link      http://www.cloudrexx.com
+ * @copyright Cloudrexx AG 2007-2015
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Cloudrexx" is a registered trademark of Cloudrexx AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ */
+
+/**
+ * Class EsiWidgetController
+ *
+ * @copyright   CLOUDREXX CMS - Cloudrexx AG Thun
+ * @author      Mirjam Doyon <info@cloudrexx.com>
+ * @package     cloudrexx
+ * @subpackage  modules_newsletter
+ */
+
+namespace Cx\Modules\Newsletter\Controller;
+
+class EsiWidgetController extends \Cx\Core_Modules\Widget\Controller\EsiWidgetController
+{
+
+    /**
+     * Parses a widget
+     *
+     * @param string $name Widget name
+     * @param \Cx\Core\Html\Sigma $template Widget Template
+     * @param \Cx\Core\Routing\Model\Entity\Response $response Response object
+     * @param array $params Get parameters
+     */
+    public function parseWidget($name, $template, $response, $params)
+    {
+
+        $newsletter = new \Cx\Modules\Newsletter\Controller\Newsletter;
+        $newsletterLib = new \Cx\Modules\Newsletter\Controller\NewsletterLib;
+
+        $profileNewsletter = $newsletter ->_profile();
+        $type = $newsletterLib-> $type;
+
+
+
+        switch ($name) {
+            case 'newsletter_form':
+                if ($type != true){
+                    $this-> $profileNewsletter;
+                } else {
+                    $this -> $profileNewsletter;
+                }
+
+                $template->parse('newsletter_list');
+                break;
+        }
+    }
+}
