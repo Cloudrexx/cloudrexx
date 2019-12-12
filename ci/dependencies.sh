b1bddd43140ec8dc7a932aff4e3070982d0e97d4
Hava <hava.fuga@comvatino.com>
2019-12-12 08:34:27 +0100

    fix(pipelines): install gcc via pip (CLX-3089)

--- ci/dependencies.sh
+++ ci/dependencies.sh
@@ -5,6 +5,10 @@ set -eu
 # Add python pip and bash
 apk add --no-cache py-pip bash
 
+# Install gcc via pip
+pip install gcc7
+gcc -v
+
 # Install docker-compose via pip
 pip install --no-cache-dir docker-compose
 docker-compose -v
