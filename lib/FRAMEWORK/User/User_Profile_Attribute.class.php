5c8276cf9380bd577dbcfb4fc9f10525a40cd02e
Sam Hawkes <sam.hawkes@comvation.com>
2020-01-23 15:57:22 +0100

    refactor(fwuser): use doctrine to delete attribute name (CLX-2255)

--- lib/FRAMEWORK/User/User_Profile_Attribute.class.php
+++ lib/FRAMEWORK/User/User_Profile_Attribute.class.php
@@ -1344,7 +1344,7 @@ DBG::log("User_Profile_Attribute::loadCoreAttributes(): Attribute $attributeId, 
 
     function deleteAttributeNames($attributeId)
     {
-        global $objDatabase, $_ARRAYLANG;
+        global $_ARRAYLANG;
 
         if (
             $this->parent_id == 'title' &&
@@ -1353,11 +1353,22 @@ DBG::log("User_Profile_Attribute::loadCoreAttributes(): Attribute $attributeId, 
             $attributeId = $pattern[0];
         }
 
-        if ($objDatabase->Execute("DELETE FROM `".DBPREFIX."access_user_attribute_name` WHERE `attribute_id` = '".$attributeId."'") !== false) {
-            return true;
+        $cx = \Cx\Core\Core\Controller\Cx::instanciate();
+        $em = $cx->getDb()->getEntityManager();
+        $attrNames = $em->getRepository(
+            'Cx\Core\User\Model\Entity\UserAttributeName'
+        )->findBy(array('attributeId' => $attributeId));
+
+        foreach ($attrNames as $attrName) {
+            $em->remove($attrName);
         }
-        $this->errorMsg = sprintf($_ARRAYLANG['TXT_ACCESS_UNABLE_DEL_ATTRIBUTE_DESCS'], htmlentities($this->arrAttributes[$attributeId]['names'][$this->langId], ENT_QUOTES, CONTREXX_CHARSET));
-        return false;
+        try {
+            $em->flush();
+        } catch (\Doctrine\ORM\OptimisticLockException $e) {
+            $this->errorMsg = sprintf($_ARRAYLANG['TXT_ACCESS_UNABLE_DEL_ATTRIBUTE_DESCS'], htmlentities($this->arrAttributes[$attributeId]['names'][$this->langId], ENT_QUOTES, CONTREXX_CHARSET));
+            return false;
+        }
+        return true;
     }
 
 
