f0dfcc0c2864cfbddc30080cf5163b20a7f65f88
Thomas Däppen <thomas.daeppen@cloudrexx.com>
2020-03-06 13:45:08 +0100

    fix(country): drop deprecated feature to manage countries (CLX-2932)

--- core/Country/Controller/Country.class.php
+++ core/Country/Controller/Country.class.php
@@ -50,11 +50,6 @@ namespace Cx\Core\Country\Controller;
 class Country
 {
     /**
-     * Text key
-     */
-    const TEXT_NAME = 'core_country_name';
-
-    /**
      * Array of all countries
      * @var     array
      * @access  private
@@ -453,187 +448,6 @@ class Country
     }
 
     /**
-     * Returns true if the record for the given ID exists in the database
-     *
-     * Returns true if the $country_id argument is empty.
-     * Returns false both if the ID cannot be found and on failure.
-     * @param   integer   $country_id   The Country ID
-     * @return  boolean                 True if the Country ID is present,
-     *                                  false otherwise.
-     */
-    static function recordExists($country_id)
-    {
-        global $objDatabase;
-
-        if (empty($country_id)) {
-            return false;
-        }
-        $objResult = $objDatabase->Execute("
-            SELECT 1
-              FROM `".DBPREFIX."core_country` AS `country`
-             WHERE `country`.`id`=$country_id");
-        if (!$objResult) return self::errorHandler();
-        return $objResult->EOF;
-    }
-
-
-    /**
-     * Stores a Country in the database.
-     *
-     * Decides whether to call {@see insert()} or {@see update()}
-     * by means of calling {@see getById()} if $country_id is valid.
-     * Optional values equal to null are ignored and not updated,
-     * or inserted with default values.
-     * Note, however, that $country_name, $alpha2 and $alpha3 are mandatory
-     * and must be non-empty when a new record is to be {@see insert()}ed!
-     * @param   string    $alpha2         The ISO 2-character code, or null
-     * @param   string    $alpha3         The ISO 3-character code, or null
-     * @param   integer   $country_name   The name of the Country, or null
-     * @param   integer   $ord            The ordinal value, or null
-     * @param   boolean   $active         The active status, or null
-     * @param   integer   $country_id     The Country ID, or null
-     * @return  boolean                   True on success, false otherwise
-     */
-    static function store(
-        $alpha2=null, $alpha3=null, $lang_id=null, $country_name=null,
-        $ord=null, $active=null, $country_id=null
-    ) {
-        $arrCountry = false;
-        if ($country_id) $arrCountry = self::getById($country_id, $lang_id);
-        if ($arrCountry) {
-//DBG::log("Country::store($alpha2, $alpha3, $lang_id, $country_name, $ord, $active, $country_id): Updating Country ID $country_id, $country_name");
-            if (!self::update($country_id, $ord, $active, $alpha2, $alpha3))
-                return false;
-        } else {
-            $country_id = self::insert(
-                $alpha2, $alpha3, $ord, $active, $country_id);
-            if (!$country_id) {
-//DBG::log("Country::store($alpha2, $alpha3, $lang_id, $country_name, $ord, $active, $country_id): Failed to insert Country ID $country_id, $country_name");
-                return false;
-            }
-        }
-        // Store the Country name only if it's set
-        if ($country_name != '') {
-            if (\Text::replace(
-                $country_id, $lang_id, 'core', self::TEXT_NAME, $country_name)) {
-//DBG::log("Country::store($alpha2, $alpha3, $lang_id, $country_name, $ord, $active, $country_id): Successfully stored Text);
-                return true;
-            }
-        }
-//DBG::log("Country::store($alpha2, $alpha3, $lang_id, $country_name, $ord, $active, $country_id): Failed to store Text");
-        // TODO: If inserting fails, the record could be rolled back
-        return false;
-    }
-
-
-    /**
-     * Inserts the Country into the database
-     *
-     * Note that the Country name is inserted or updated in {@see store()} only.
-     * @param   string    $alpha2     The ISO 2-character code
-     * @param   string    $alpha3     The ISO 3-character code
-     * @param   integer   $ord        The ordinal value, or null
-     * @param   boolean   $active     The active status, or null
-     * @param   integer   $country_id The Country ID, or null
-     * @return  boolean               True on success, false otherwise
-     */
-    static function insert(
-        $alpha2, $alpha3, $ord=null, $active=null, $country_id=null
-    ) {
-        global $objDatabase;
-
-        if (empty($alpha2) || empty($alpha3)) {
-//DBG::log("Country::insert(): Error: Trying to store Country with empty name or alpha code");
-            return false;
-        }
-        $objResult = $objDatabase->Execute("
-            INSERT INTO `".DBPREFIX."core_country` (
-              `id`,
-              `alpha2`,
-              `alpha3`,
-              `ord`,
-              `active`
-            ) VALUES (
-              ".($country_id ? $country_id : 'NULL').",
-              '".addslashes($alpha2)."',
-              '".addslashes($alpha3)."',
-              ".intval($ord).",
-              ".intval($active)."
-            )");
-        if (!$objResult) return false; //self::errorHandler();
-        return ($country_id ? $country_id : $objDatabase->Insert_ID());
-    }
-
-
-    /**
-     * Updates the Country in the database
-     *
-     * Note that it should never be necessary to update the Text ID,
-     * so that parameter is not present here.  Call {@see store()}
-     * to update the Country name as well.
-     * @param   integer   $country_id The Country ID
-     * @param   integer   $ord        The ordinal value, or null
-     * @param   boolean   $active     The active status, or null
-     * @param   string    $alpha2     The ISO 2-character code, or null
-     * @param   string    $alpha3     The ISO 3-character code, or null
-     * @return  boolean               True on success, false otherwise
-     */
-    static function update(
-        $country_id, $ord=null, $active=null, $alpha2=null, $alpha3=null
-    ) {
-        global $objDatabase;
-
-        if (empty($country_id)) {
-//die("Country::update($country_id, $text_id, $ord, $active, $alpha2, $alpha3): Error: Cannot update without a valid Country ID");
-            return false;
-        }
-        $query = array();
-        if (isset($ord))     $query[] = "`ord`=$ord";
-        if (isset($active))  $query[] = "`active`=".intval($active);
-        if (!empty($alpha2)) $query[] = "`alpha2`='".addslashes($alpha2)."'";
-        if (!empty($alpha3)) $query[] = "`alpha3`='".addslashes($alpha3)."'";
-        // Something to do?
-        if ($query) {
-            $objResult = $objDatabase->Execute("
-                UPDATE `".DBPREFIX."core_country`
-                   SET ".join(', ', $query)."
-                 WHERE `id`=$country_id");
-            if (!$objResult) return false; //self::errorHandler();
-        }
-        return true;
-    }
-
-
-    /**
-     * Deletes the Country with the given ID from the database
-     * @param   integer   $country_id The Country ID
-     * @return  boolean               True on success, false otherwise
-     */
-    static function deleteById($country_id)
-    {
-        global $objDatabase, $_CORELANG;
-
-        if (is_null(self::$arrCountries)) self::init();
-        if (empty(self::$arrCountries[$country_id])) {
-//            Message::add($_CORELANG['TXT_CORE_COUNTRY_ERROR_DELETING_NOT_FOUND'];
-            return false;
-        }
-        if (!\Text::deleteById($country_id, 'core', self::TEXT_NAME, 0)) {
-            return false;
-        }
-        $query = "
-            DELETE FROM `".DBPREFIX."core_country`
-             WHERE `id`=".intval($country_id);
-        $objResult = $objDatabase->Execute($query);
-        if (!$objResult) {
-//            Message::add($_CORELANG['TXT_CORE_COUNTRY_ERROR_DELETING'];
-            return false;
-        }
-        return true;
-    }
-
-
-    /**
      * Returns the HTML dropdown menu or hidden input field plus name string
      *
      * If there is just one active country, returns a hidden <input> tag with
@@ -723,106 +537,18 @@ class Country
             'active' => array('type' => 'TINYINT(1)', 'unsigned' => true, 'notnull' => true, 'default' => '1', 'renamefrom' => 'is_active'),
         );
         \Cx\Lib\UpdateUtil::table($table_name, $table_structure);
+
         if (\Cx\Lib\UpdateUtil::table_empty($table_name)) {
-            \Text::deleteByKey('core', self::TEXT_NAME);
-            // Copy the Countries from the Shop module if possible
             if (\Cx\Lib\UpdateUtil::table_exist(DBPREFIX."module_shop_countries")) {
-                $query = "
-                    SELECT `countries_id`, `countries_name`,
-                           `countries_iso_code_2`, `countries_iso_code_3`,
-                           `activation_status`
-                      FROM ".DBPREFIX."module_shop_countries";
-                $objResult = \Cx\Lib\UpdateUtil::sql($query);
-                if (!$objResult) {
-                    throw new \Cx\Lib\Update_DatabaseException(
-                       "Failed to to query Country names", $query);
-                }
-                $default_lang_id = \FWLanguage::getDefaultLangId();
-                while (!$objResult->EOF) {
-                    $id = $objResult->fields['countries_id'];
-                    $name = $objResult->fields['countries_name'];
-                    $alpha2 = $objResult->fields['countries_iso_code_2'];
-                    $alpha3 = $objResult->fields['countries_iso_code_3'];
-                    $active = $objResult->fields['activation_status'];
-                    $ord = 0;
-                    if ($id == 14) {
-                        // fixing missing name
-                        $name = 'Österreich';
-                    }
-                    if (!self::store($alpha2, $alpha3, $default_lang_id,
-                        $name, $ord, $active, $id)
-                    ) {
-                        throw new \Cx\Lib\Update_DatabaseException(
-                           "Failed to to migrate Country '$name'");
-                    }
-                    $objResult->MoveNext();
-                }
                 \Cx\Lib\UpdateUtil::drop_table(DBPREFIX.'module_shop_countries');
             }
-        }
-
-
-// USE FOR NEW INSTALLATIONS ONLY!
-// These records will lead to inconsistencies with Country references in
-// other tables otherwise.
-        if (\Cx\Lib\UpdateUtil::table_empty($table_name)) {
-            // Add new Country records if available
-            if (   file_exists(ASCMS_CORE_PATH.'/countries_iso_3166-2.php')
-                && include_once(ASCMS_CORE_PATH.'/countries_iso_3166-2.php')) {
-//DBG::log("Country::errorHandler(): Included ISO file");
-                $arrCountries = null;
-                $ord = 0;
-                foreach ($arrCountries as $country_id => $arrCountry) {
-                    $name = $arrCountry[0];
-                    $alpha2 = $arrCountry[1];
-                    $alpha3 = $arrCountry[2];
-// Not currently in use:
-//                    $numeric = $arrCountry[3];
-//                    $iso_full = $arrCountry[4];
-                    // English (language ID 2) only!
-                    if (!self::store(
-                        $alpha2, $alpha3, 2, $name, ++$ord, true, $country_id)
-                    ) {
-                        throw new \Cx\Lib\Update_DatabaseException(
-                           "Failed to to add Country '$name' from ISO file");
-                    }
-//DBG::log("Country::errorHandler(): Added Country ID $country_id: '$name'");
-                }
-            }
-        }
-
-//DBG::activate(DBG_ADODB);
-        // Add more languages from the countries_languages.php file,
-        // if present
-        $arrCountries = array();
-        // $arrCountries is redefined in the file
-        if (   file_exists(ASCMS_CORE_PATH.'/countries_languages.php')
-            && include_once ASCMS_CORE_PATH.'/countries_languages.php') {
-            foreach ($arrCountries as $alpha2 => $arrLanguage) {
-//DBG::log("errorHandler: Looking for Alpha-2 $alpha2");
-                $country_id = self::getIdByAlpha2($alpha2);
-                if (!$country_id) {
-// TODO: Fail or not?
-                    continue;
-                }
-                foreach ($arrLanguage as $lang_id => $name) {
-                    if (!\Text::replace($country_id, $lang_id, 'core',
-                        self::TEXT_NAME, $name)) {
-                        throw new \Cx\Lib\Update_DatabaseException(
-                           "Failed to to update Country '$name' from languages file");
-                    }
-//DBG::log("Country::errorHandler(): Added Country ID $country_id: language ID $lang_id");
-                }
-            }
+            // TODO: init country list
         }
 
         \Cx\Core\Setting\Controller\Setting::init('core', 'country');
         \Cx\Core\Setting\Controller\Setting::add('numof_countries_per_page_backend', 30, 101);
 
-        // More to come...
-
         // Always!
         return false;
     }
-
 }
