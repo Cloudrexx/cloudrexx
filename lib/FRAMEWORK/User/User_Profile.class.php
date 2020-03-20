5e0c2027be2229db29d59a57077a2046bbe2fd59
Sam Hawkes <sam.hawkes@comvation.com>
2020-03-20 15:53:39 +0100

    fix(lib): store attribute value correctly (CLX-2454)

--- lib/FRAMEWORK/User/User_Profile.class.php
+++ lib/FRAMEWORK/User/User_Profile.class.php
@@ -232,8 +232,6 @@ class User_Profile
             {
                 $newValue = !isset($this->arrCachedUsers[$this->id]['profile'][$attributeId][$historyId]);
                 if ($newValue || $value != $this->arrCachedUsers[$this->id]['profile'][$attributeId][$historyId]) {
-                    $value = '"' . contrexx_raw2db($value) . '"';
-
                     if ($this->objAttribute->isDefaultAttribute($attributeId)) {
                         $attributeId = $this->objAttribute->getAttributeIdByDefaultAttributeId($attributeId);
                     }
@@ -251,7 +249,7 @@ class User_Profile
                     $attributeValue->setUserId($this->id);
                     $attributeValue->setUser($user);
                     $attributeValue->setHistory($historyId);
-                    $attributeValue->setValue($value);
+                    $attributeValue->setValue(contrexx_raw2db($value));
 
                     try {
                         $em->persist($attributeValue);
