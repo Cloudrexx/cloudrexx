e5fd45562bd30f5ab1b33acef9cce8f4d62a47a8
Sam Hawkes <sam.hawkes@comvation.com>
2020-04-22 10:12:38 +0200

    style(newsletter): correct intention error (CLX-2255)

--- modules/Newsletter/Controller/NewsletterManager.class.php
+++ modules/Newsletter/Controller/NewsletterManager.class.php
@@ -2749,8 +2749,8 @@ class NewsletterManager extends NewsletterLib
                 foreach ($users as $user) {
                     // Check if the received user is correct
                     if (!empty($usersToCheck) && !in_array($user->getId(), $usersToCheck)) {
-                	    continue;
-	                }
+                        continue;
+                    }
 
                     $this->simulateDistinct($distinctByType, $user->getEmail(), $type, $mailRecipients);
                 }
