ca3c9788181d3337f6313482a6f73a8500c18d98
Sam Hawkes <sam.hawkes@comvation.com>
2020-04-30 14:21:06 +0200

    refactor(user): translate user attribute names (CLX-2454)

--- core/User/Data/Migration.sql
+++ core/User/Data/Migration.sql
@@ -458,20 +458,406 @@ UPDATE `contrexx_access_user_attribute` AS a1
     SET a1.context = a2.name
     WHERE a1.is_default = 1;
 
+/** Define attribute keys as variables **/
+SELECT @picture := id FROM contrexx_access_user_attribute WHERE context = 'picture';
+SELECT @gender := id FROM contrexx_access_user_attribute WHERE context = 'gender';
+SELECT @title := id FROM contrexx_access_user_attribute WHERE context = 'title';
+SELECT @designation := id FROM contrexx_access_user_attribute WHERE context = 'designation';
+SELECT @firstname := id FROM contrexx_access_user_attribute WHERE context = 'firstname';
+SELECT @lastname := id FROM contrexx_access_user_attribute WHERE context = 'lastname';
+SELECT @company := id FROM contrexx_access_user_attribute WHERE context = 'company';
+SELECT @address := id FROM contrexx_access_user_attribute WHERE context = 'address';
+SELECT @city := id FROM contrexx_access_user_attribute WHERE context = 'city';
+SELECT @country := id FROM contrexx_access_user_attribute WHERE context = 'country';
+SELECT @zip := id FROM contrexx_access_user_attribute WHERE context = 'zip';
+SELECT @phone_office := id FROM contrexx_access_user_attribute WHERE context = 'phone_office';
+SELECT @phone_private := id FROM contrexx_access_user_attribute WHERE context = 'phone_private';
+SELECT @phone_mobile := id FROM contrexx_access_user_attribute WHERE context = 'phone_mobile';
+SELECT @phone_fax := id FROM contrexx_access_user_attribute WHERE context = 'phone_fax';
+SELECT @birthday := id FROM contrexx_access_user_attribute WHERE context = 'birthday';
+SELECT @website := id FROM contrexx_access_user_attribute WHERE context = 'website';
+SELECT @profession := id FROM contrexx_access_user_attribute WHERE context = 'profession';
+SELECT @interests := id FROM contrexx_access_user_attribute WHERE context = 'interests';
+SELECT @signature := id FROM contrexx_access_user_attribute WHERE context = 'signature';
+SELECT @gender_female := id FROM contrexx_access_user_attribute WHERE context = 'gender_female';
+SELECT @gender_male := id FROM contrexx_access_user_attribute WHERE context = 'gender_male';
+
+/** Add default attribute translations **/
 INSERT INTO `contrexx_translations` (`locale`, `object_class`, `field`, `foreign_key`, `content`)
 SELECT
-	(
-	    CASE
-	        WHEN `a`.`lang_id` = 3 THEN 'fr'
-            WHEN `l`.`iso_1` IS NULL THEN 'de'
-            ELSE `l`.`iso_1`
-        END
-    ) AS `locale`,
+	`iso_1` AS `locale`,
+    'Cx\\Core\\User\\Model\\Entity\\UserAttribute' AS `object_class`,
+    'name' AS `field`,
+    @picture AS `foreign_key`,
+    CASE
+        WHEN `source_language` = 'da' THEN 'Profilbillede'
+        WHEN `source_language` = 'de' THEN 'Profilbild'
+        WHEN `source_language` = 'en' THEN 'Profile image'
+        WHEN `source_language` = 'fr' THEN 'Image du profil'
+        WHEN `source_language` = 'it' THEN 'Profilbild'
+        WHEN `source_language` = 'ru' THEN 'Profilbild'
+    END AS `content`
+FROM `contrexx_core_locale_locale`;
+
+INSERT INTO `contrexx_translations` (`locale`, `object_class`, `field`, `foreign_key`, `content`)
+SELECT
+	`iso_1` AS `locale`,
+    'Cx\\Core\\User\\Model\\Entity\\UserAttribute' AS `object_class`,
+    'name' AS `field`,
+    @gender AS `foreign_key`,
+    CASE
+        WHEN `source_language` = 'da' THEN 'Køn'
+        WHEN `source_language` = 'de' THEN 'Geschlecht'
+        WHEN `source_language` = 'en' THEN 'Gender'
+        WHEN `source_language` = 'fr' THEN 'Geschlecht'
+        WHEN `source_language` = 'it' THEN 'sesso'
+        WHEN `source_language` = 'ru' THEN 'Geschlecht'
+    END AS `content`
+FROM `contrexx_core_locale_locale`;
+
+INSERT INTO `contrexx_translations` (`locale`, `object_class`, `field`, `foreign_key`, `content`)
+SELECT
+	`iso_1` AS `locale`,
+    'Cx\\Core\\User\\Model\\Entity\\UserAttribute' AS `object_class`,
+    'name' AS `field`,
+    @title AS `foreign_key`,
+    CASE
+        WHEN `source_language` = 'da' THEN 'Tiltale'
+        WHEN `source_language` = 'de' THEN 'Anrede'
+        WHEN `source_language` = 'en' THEN 'Salutation'
+        WHEN `source_language` = 'fr' THEN 'Civilité'
+        WHEN `source_language` = 'it' THEN 'Anrede'
+        WHEN `source_language` = 'ru' THEN 'Anrede'
+    END AS `content`
+FROM `contrexx_core_locale_locale`;
+
+INSERT INTO `contrexx_translations` (`locale`, `object_class`, `field`, `foreign_key`, `content`)
+SELECT
+	`iso_1` AS `locale`,
+    'Cx\\Core\\User\\Model\\Entity\\UserAttribute' AS `object_class`,
+    'name' AS `field`,
+    @designation AS `foreign_key`,
+    CASE
+        WHEN `source_language` = 'da' THEN 'Titel'
+        WHEN `source_language` = 'de' THEN 'Titel'
+        WHEN `source_language` = 'en' THEN 'Designation'
+        WHEN `source_language` = 'fr' THEN 'Titel'
+        WHEN `source_language` = 'it' THEN 'Titel'
+        WHEN `source_language` = 'ru' THEN 'Titel'
+    END AS `content`
+FROM `contrexx_core_locale_locale`;
+
+INSERT INTO `contrexx_translations` (`locale`, `object_class`, `field`, `foreign_key`, `content`)
+SELECT
+	`iso_1` AS `locale`,
+    'Cx\\Core\\User\\Model\\Entity\\UserAttribute' AS `object_class`,
+    'name' AS `field`,
+    @firstname AS `foreign_key`,
+    CASE
+        WHEN `source_language` = 'da' THEN 'Fornavn'
+        WHEN `source_language` = 'de' THEN 'Vorname'
+        WHEN `source_language` = 'en' THEN 'First name'
+        WHEN `source_language` = 'fr' THEN 'Prénom'
+        WHEN `source_language` = 'it' THEN 'Nome'
+        WHEN `source_language` = 'ru' THEN 'Имя'
+    END AS `content`
+FROM `contrexx_core_locale_locale`;
+
+INSERT INTO `contrexx_translations` (`locale`, `object_class`, `field`, `foreign_key`, `content`)
+SELECT
+	`iso_1` AS `locale`,
+    'Cx\\Core\\User\\Model\\Entity\\UserAttribute' AS `object_class`,
+    'name' AS `field`,
+    @lastname AS `foreign_key`,
+    CASE
+        WHEN `source_language` = 'da' THEN 'Efternavn'
+        WHEN `source_language` = 'de' THEN 'Nachname'
+        WHEN `source_language` = 'en' THEN 'Last name'
+        WHEN `source_language` = 'fr' THEN 'Nom'
+        WHEN `source_language` = 'it' THEN 'Cognome'
+        WHEN `source_language` = 'ru' THEN 'Фамилия'
+    END AS `content`
+FROM `contrexx_core_locale_locale`;
+
+INSERT INTO `contrexx_translations` (`locale`, `object_class`, `field`, `foreign_key`, `content`)
+SELECT
+	`iso_1` AS `locale`,
+    'Cx\\Core\\User\\Model\\Entity\\UserAttribute' AS `object_class`,
+    'name' AS `field`,
+    @company AS `foreign_key`,
+    CASE
+        WHEN `source_language` = 'da' THEN 'Firma'
+        WHEN `source_language` = 'de' THEN 'Firma'
+        WHEN `source_language` = 'en' THEN 'Company'
+        WHEN `source_language` = 'fr' THEN 'Entreprise'
+        WHEN `source_language` = 'it' THEN 'Firma'
+        WHEN `source_language` = 'ru' THEN 'Firma'
+    END AS `content`
+FROM `contrexx_core_locale_locale`;
+
+INSERT INTO `contrexx_translations` (`locale`, `object_class`, `field`, `foreign_key`, `content`)
+SELECT
+	`iso_1` AS `locale`,
+    'Cx\\Core\\User\\Model\\Entity\\UserAttribute' AS `object_class`,
+    'name' AS `field`,
+    @address AS `foreign_key`,
+    CASE
+        WHEN `source_language` = 'da' THEN 'Adresse'
+        WHEN `source_language` = 'de' THEN 'Adresse'
+        WHEN `source_language` = 'en' THEN 'Address'
+        WHEN `source_language` = 'fr' THEN 'Adresse'
+        WHEN `source_language` = 'it' THEN 'Adresse'
+        WHEN `source_language` = 'ru' THEN 'Adresse'
+    END AS `content`
+FROM `contrexx_core_locale_locale`;
+
+INSERT INTO `contrexx_translations` (`locale`, `object_class`, `field`, `foreign_key`, `content`)
+SELECT
+	`iso_1` AS `locale`,
+    'Cx\\Core\\User\\Model\\Entity\\UserAttribute' AS `object_class`,
+    'name' AS `field`,
+    @city AS `foreign_key`,
+    CASE
+        WHEN `source_language` = 'da' THEN 'By'
+        WHEN `source_language` = 'de' THEN 'Ort'
+        WHEN `source_language` = 'en' THEN 'City'
+        WHEN `source_language` = 'fr' THEN 'Lieu'
+        WHEN `source_language` = 'it' THEN 'Ort'
+        WHEN `source_language` = 'ru' THEN 'Ort'
+    END AS `content`
+FROM `contrexx_core_locale_locale`;
+
+INSERT INTO `contrexx_translations` (`locale`, `object_class`, `field`, `foreign_key`, `content`)
+SELECT
+	`iso_1` AS `locale`,
+    'Cx\\Core\\User\\Model\\Entity\\UserAttribute' AS `object_class`,
+    'name' AS `field`,
+    @country AS `foreign_key`,
+    CASE
+        WHEN `source_language` = 'da' THEN 'Land'
+        WHEN `source_language` = 'de' THEN 'Land'
+        WHEN `source_language` = 'en' THEN 'Country'
+        WHEN `source_language` = 'fr' THEN 'Pays'
+        WHEN `source_language` = 'it' THEN 'Land'
+        WHEN `source_language` = 'ru' THEN 'Land'
+    END AS `content`
+FROM `contrexx_core_locale_locale`;
+
+INSERT INTO `contrexx_translations` (`locale`, `object_class`, `field`, `foreign_key`, `content`)
+SELECT
+	`iso_1` AS `locale`,
+    'Cx\\Core\\User\\Model\\Entity\\UserAttribute' AS `object_class`,
+    'name' AS `field`,
+    @zip AS `foreign_key`,
+    CASE
+        WHEN `source_language` = 'da' THEN 'Postnr.'
+        WHEN `source_language` = 'de' THEN 'PLZ'
+        WHEN `source_language` = 'en' THEN 'ZIP'
+        WHEN `source_language` = 'fr' THEN 'NPA'
+        WHEN `source_language` = 'it' THEN 'PLZ'
+        WHEN `source_language` = 'ru' THEN 'Zip'
+    END AS `content`
+FROM `contrexx_core_locale_locale`;
+
+INSERT INTO `contrexx_translations` (`locale`, `object_class`, `field`, `foreign_key`, `content`)
+SELECT
+	`iso_1` AS `locale`,
+    'Cx\\Core\\User\\Model\\Entity\\UserAttribute' AS `object_class`,
+    'name' AS `field`,
+    @phone_office AS `foreign_key`,
+    CASE
+        WHEN `source_language` = 'da' THEN 'Tel. Arbejde'
+        WHEN `source_language` = 'de' THEN 'Tel. Büro'
+        WHEN `source_language` = 'en' THEN 'Office phone'
+        WHEN `source_language` = 'fr' THEN 'Tel. Bureau'
+        WHEN `source_language` = 'it' THEN 'Tel. Büro'
+        WHEN `source_language` = 'ru' THEN 'Tel. Büro'
+    END AS `content`
+FROM `contrexx_core_locale_locale`;
+
+INSERT INTO `contrexx_translations` (`locale`, `object_class`, `field`, `foreign_key`, `content`)
+SELECT
+	`iso_1` AS `locale`,
+    'Cx\\Core\\User\\Model\\Entity\\UserAttribute' AS `object_class`,
+    'name' AS `field`,
+    @phone_private AS `foreign_key`,
+    CASE
+        WHEN `source_language` = 'da' THEN 'Tel. Privat'
+        WHEN `source_language` = 'de' THEN 'Tel. Privat'
+        WHEN `source_language` = 'en' THEN 'Private phone'
+        WHEN `source_language` = 'fr' THEN 'Tel. Privé'
+        WHEN `source_language` = 'it' THEN 'Tel. Privat'
+        WHEN `source_language` = 'ru' THEN 'Tel. Privat'
+    END AS `content`
+FROM `contrexx_core_locale_locale`;
+
+INSERT INTO `contrexx_translations` (`locale`, `object_class`, `field`, `foreign_key`, `content`)
+SELECT
+	`iso_1` AS `locale`,
+    'Cx\\Core\\User\\Model\\Entity\\UserAttribute' AS `object_class`,
+    'name' AS `field`,
+    @phone_mobile AS `foreign_key`,
+    CASE
+        WHEN `source_language` = 'da' THEN 'Tel. Mobil'
+        WHEN `source_language` = 'de' THEN 'Tel. Mobile'
+        WHEN `source_language` = 'en' THEN 'Mobile phone'
+        WHEN `source_language` = 'fr' THEN 'Tel. Mobile'
+        WHEN `source_language` = 'it' THEN 'Tel. Mobile'
+        WHEN `source_language` = 'ru' THEN 'Tel. Mobile'
+    END AS `content`
+FROM `contrexx_core_locale_locale`;
+
+INSERT INTO `contrexx_translations` (`locale`, `object_class`, `field`, `foreign_key`, `content`)
+SELECT
+	`iso_1` AS `locale`,
+    'Cx\\Core\\User\\Model\\Entity\\UserAttribute' AS `object_class`,
+    'name' AS `field`,
+    @phone_fax AS `foreign_key`,
+    CASE
+        WHEN `source_language` = 'da' THEN 'Fax'
+        WHEN `source_language` = 'de' THEN 'Fax'
+        WHEN `source_language` = 'en' THEN 'Fax'
+        WHEN `source_language` = 'fr' THEN 'Fax'
+        WHEN `source_language` = 'it' THEN 'Fax'
+        WHEN `source_language` = 'ru' THEN 'Fax'
+    END AS `content`
+FROM `contrexx_core_locale_locale`;
+
+INSERT INTO `contrexx_translations` (`locale`, `object_class`, `field`, `foreign_key`, `content`)
+SELECT
+	`iso_1` AS `locale`,
+    'Cx\\Core\\User\\Model\\Entity\\UserAttribute' AS `object_class`,
+    'name' AS `field`,
+    @birthday AS `foreign_key`,
+    CASE
+        WHEN `source_language` = 'da' THEN 'Fødselsdag'
+        WHEN `source_language` = 'de' THEN 'Geburtstag'
+        WHEN `source_language` = 'en' THEN 'Birthday'
+        WHEN `source_language` = 'fr' THEN 'Date de naissance'
+        WHEN `source_language` = 'it' THEN 'Geburtstag'
+        WHEN `source_language` = 'ru' THEN 'Geburtstag'
+    END AS `content`
+FROM `contrexx_core_locale_locale`;
+
+INSERT INTO `contrexx_translations` (`locale`, `object_class`, `field`, `foreign_key`, `content`)
+SELECT
+	`iso_1` AS `locale`,
+    'Cx\\Core\\User\\Model\\Entity\\UserAttribute' AS `object_class`,
+    'name' AS `field`,
+    @website AS `foreign_key`,
+    CASE
+        WHEN `source_language` = 'da' THEN 'Hjemmeside'
+        WHEN `source_language` = 'de' THEN 'Webseite'
+        WHEN `source_language` = 'en' THEN 'Website'
+        WHEN `source_language` = 'fr' THEN 'Site WEB'
+        WHEN `source_language` = 'it' THEN 'Webseite'
+        WHEN `source_language` = 'ru' THEN 'Webseite'
+    END AS `content`
+FROM `contrexx_core_locale_locale`;
+
+INSERT INTO `contrexx_translations` (`locale`, `object_class`, `field`, `foreign_key`, `content`)
+SELECT
+	`iso_1` AS `locale`,
+    'Cx\\Core\\User\\Model\\Entity\\UserAttribute' AS `object_class`,
+    'name' AS `field`,
+    @profession AS `foreign_key`,
+    CASE
+        WHEN `source_language` = 'da' THEN 'Profession'
+        WHEN `source_language` = 'de' THEN 'Beruf'
+        WHEN `source_language` = 'en' THEN 'Profession'
+        WHEN `source_language` = 'fr' THEN 'Métier'
+        WHEN `source_language` = 'it' THEN 'Beruf'
+        WHEN `source_language` = 'ru' THEN 'Beruf'
+    END AS `content`
+FROM `contrexx_core_locale_locale`;
+
+INSERT INTO `contrexx_translations` (`locale`, `object_class`, `field`, `foreign_key`, `content`)
+SELECT
+	`iso_1` AS `locale`,
+    'Cx\\Core\\User\\Model\\Entity\\UserAttribute' AS `object_class`,
+    'name' AS `field`,
+    @interests AS `foreign_key`,
+    CASE
+        WHEN `source_language` = 'da' THEN 'Interesser'
+        WHEN `source_language` = 'de' THEN 'Interessen'
+        WHEN `source_language` = 'en' THEN 'Interests'
+        WHEN `source_language` = 'fr' THEN 'Domaines d intérêts'
+        WHEN `source_language` = 'it' THEN 'Interessen'
+        WHEN `source_language` = 'ru' THEN 'Interessen'
+    END AS `content`
+FROM `contrexx_core_locale_locale`;
+
+INSERT INTO `contrexx_translations` (`locale`, `object_class`, `field`, `foreign_key`, `content`)
+SELECT
+	`iso_1` AS `locale`,
+    'Cx\\Core\\User\\Model\\Entity\\UserAttribute' AS `object_class`,
+    'name' AS `field`,
+    @signature AS `foreign_key`,
+    CASE
+        WHEN `source_language` = 'da' THEN 'Underskrift'
+        WHEN `source_language` = 'de' THEN 'Signatur'
+        WHEN `source_language` = 'en' THEN 'Signature'
+        WHEN `source_language` = 'fr' THEN 'Signature'
+        WHEN `source_language` = 'it' THEN 'Signatur'
+        WHEN `source_language` = 'ru' THEN 'Signatur'
+    END AS `content`
+FROM `contrexx_core_locale_locale`;
+
+INSERT INTO `contrexx_translations` (`locale`, `object_class`, `field`, `foreign_key`, `content`)
+SELECT
+	`iso_1` AS `locale`,
+    'Cx\\Core\\User\\Model\\Entity\\UserAttribute' AS `object_class`,
+    'name' AS `field`,
+    @gender_female AS `foreign_key`,
+    CASE
+        WHEN `source_language` = 'da' THEN 'Kvindelig'
+        WHEN `source_language` = 'de' THEN 'Weiblich'
+        WHEN `source_language` = 'en' THEN 'Female'
+        WHEN `source_language` = 'fr' THEN 'Féminin'
+        WHEN `source_language` = 'it' THEN 'Femmina'
+        WHEN `source_language` = 'ru' THEN 'Weiblich'
+    END AS `content`
+FROM `contrexx_core_locale_locale`;
+
+INSERT INTO `contrexx_translations` (`locale`, `object_class`, `field`, `foreign_key`, `content`)
+SELECT
+	`iso_1` AS `locale`,
+    'Cx\\Core\\User\\Model\\Entity\\UserAttribute' AS `object_class`,
+    'name' AS `field`,
+    @gender_male AS `foreign_key`,
+    CASE
+        WHEN `source_language` = 'da' THEN 'Mand'
+        WHEN `source_language` = 'de' THEN 'Männlich'
+        WHEN `source_language` = 'en' THEN 'Male'
+        WHEN `source_language` = 'fr' THEN 'Masculin'
+        WHEN `source_language` = 'it' THEN 'Maschio'
+        WHEN `source_language` = 'ru' THEN 'Мужской'
+    END AS `content`
+FROM `contrexx_core_locale_locale`;
+
+/** Take over all user attribute names that can be assigned to a language **/
+INSERT INTO `contrexx_translations` (`locale`, `object_class`, `field`, `foreign_key`, `content`)
+SELECT
+	`l`.`iso_1` AS `locale`,
     'Cx\\Core\\User\\Model\\Entity\\UserAttribute' AS `object_class`,
     'name' AS `field`,
     `a`.`attribute_id` AS `foreign_key`,
     `a`.`name` AS `content`
 FROM `contrexx_access_user_attribute_name` AS `a`
-LEFT JOIN `contrexx_core_locale_locale` AS `l` ON `a`.`lang_id` = `l`.`id`;
+LEFT JOIN `contrexx_core_locale_locale` AS `l` ON `a`.`lang_id` = `l`.`id`
+WHERE `a`.`lang_id` <> 0 AND `l`.`iso_1` IS NOT NULL;
+
+/** Take over all User Attribute names that do not have a specific language in all languages **/
+INSERT INTO `contrexx_translations` (`locale`, `object_class`, `field`, `foreign_key`, `content`)
+SELECT
+	`l`.`iso_1` AS `locale`,
+    'Cx\\Core\\User\\Model\\Entity\\UserAttribute' AS `object_class`,
+    'name' AS `field`,
+    `a`.`id` AS `foreign_key`,
+    `an`.`name` AS `content`
+FROM `contrexx_access_user_attribute_name` AS `an`
+CROSS JOIN `contrexx_core_locale_locale` AS `l`
+LEFT JOIN `contrexx_access_user_attribute` AS `a` ON `a`.`id` = `an`.`attribute_id`
+WHERE `a`.`is_default` = 0 AND `an`.`lang_id` = 0;
 
 DROP TABLE `contrexx_access_user_attribute_name`;
