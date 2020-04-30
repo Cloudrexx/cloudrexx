b7aac400a9785ed44eda144238c45d1ac0cee139
Sam Hawkes <sam.hawkes@comvation.com>
2020-04-30 08:58:45 +0200

    fix(lib): get hashed password (CLX-2255)

--- lib/FRAMEWORK/User/User.class.php
+++ lib/FRAMEWORK/User/User.class.php
@@ -2143,7 +2143,9 @@ class User extends User_Profile
                 $this->error_msg[] = $_CORELANG['TXT_ACCESS_PASSWORD_NOT_CONFIRMED'];
                 return false;
             }
-            $this->password = \Cx\Core\Model\Model\Entity\Password::createFromPlaintext($password);;
+            $this->password = \Cx\Core\Model\Model\Entity\Password::createFromPlaintext(
+                $password
+            )->__toString();
             $this->updateLoadedUserData('password', $this->password);
             return true;
         }
